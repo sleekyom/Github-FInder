f386858bced26d146cff805ddf92dc2bf91374c8
Emmanuel Kolawole <amdas72@gmail.com>
2021-01-05 02:14:37 +0200

    User Profile & Bio

--- src/users/User.js
+++ src/users/User.js
@@ -1,9 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../components/layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
 	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired,
+	};
 
 	render() {
 		const {
@@ -11,6 +20,7 @@ export class User extends Component {
 			avatar_url,
 			location,
 			bio,
+			company,
 			blog,
 			login,
 			html_url,
@@ -23,7 +33,69 @@ export class User extends Component {
 
 		const { loading } = this.props;
 
-		return <div>{name}</div>;
+		if (loading) return <Spinner />;
+
+		return (
+			<Fragment>
+				<Link to='/' className='btn btn-light'>
+					Back to Search
+				</Link>
+				Hireable: {""}
+				{hireable ? (
+					<i className='fas fa-check text-success' />
+				) : (
+					<i className='fas fa-times-circle text-danger' />
+				)}
+				<div className='card grid-2'>
+					<div className='all-center'>
+						<img
+							src={avatar_url}
+							alt=''
+							className='round-img'
+							style={{ width: "150px" }}
+						/>
+						<h1>{name}</h1>
+						<p>Location: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								{bio}
+							</Fragment>
+						)}
+						<a href={html_url} className='btn btn-dark my-1'>
+							Visit Github Page
+						</a>
+
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Username: </strong> {login}
+									</Fragment>
+								)}
+							</li>
+
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Company: </strong> {company}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website: </strong> {blog}
+									</Fragment>
+								)}
+							</li>
+						</ul>
+					</div>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
